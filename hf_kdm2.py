02f3855a960a16af9e1f42b844861f0c5864a429
rdcunha <rdcunha2@vt.edu>
2022-11-03 12:08:53 -0500

    add qksd as state prep script

--- hf_kdm2.py
+++ hf_kdm2.py
@@ -54,7 +54,7 @@ tau=args.tau
 #         H 5.0 0.0 0.0
 #         H 6.0 0.0 0.0'''
 xyz = '''H 0.0 0.0 0.0
-         H  0.0 0.0 0.5
+         H  0.0 0.0 0.6
          H  0.0 0.0 1.5
          H  0.0 0.0 2.0'''
 
@@ -64,18 +64,23 @@ mol = gto.M (atom = xyz, basis = 'sto-3g', output='h4_sto3g.log',
 
 # Do RHF
 mf = scf.RHF(mol).run()
+
+# The LASSCF here is needed so the orbital space is the localized
+# active space
 las = LASSCF(mf, (2,2),(2,2), spin_sub=(1,1))
 frag_atom_list = ((0,1),(2,3))
 loc_mo_coeff = las.localize_init_guess(frag_atom_list, mf.mo_coeff)
-#las.kernel(loc_mo_coeff)
+las.kernel(loc_mo_coeff)
 loc_mo_coeff = las.mo_coeff
 print("LASSCF energy: ", las.e_tot)
 nuc_rep_en = las.energy_nuc()
+print("Nuclear repulsion: ", las.energy_nuc())
 ncas_sub = las.ncas_sub
 nelecas_sub = las.nelecas_sub
 
 ## Fragment QKSD
-## Load the LAS wave function using QKSD
+## Use QKSD together with the LAS fragment Hamiltonians
+## to give better fragment wavefunctions
 
 h1_las = las.h1e_for_cas()
 eri_las = las.get_h2eff(loc_mo_coeff)
@@ -93,7 +98,9 @@ for f in range(len(ncas_sub)):
     print("H1_frag shape: ", h1_frag[f].shape)
     print("H2_frag shape: ", h2_frag[f].shape)
 
+coeff_list = []
 wfn_list = []
+ham_list = []
 
 for frag in range(len(ncas_sub)):
     n_so = 2*ncas_sub[frag]
@@ -147,11 +154,13 @@ for frag in range(len(ncas_sub)):
     # U |\phi_0>
     statevector = apply_time_evolution_op(init_statevector, Hsp, tau, time_steps)
     omega_list = [np.asarray(state, dtype=complex) for state in statevector]
+    wfn_list.append(omega_list)
     
-    print("Norm of Krylov basis states: ",np.linalg.norm(omega_list[0]))
+    print("Norm of Krylov basis states: ",np.linalg.norm(omega_list[1]))
 
     # V U |\phi_0>
     Homega_list = [Hsp.dot(omega) for omega in omega_list]
+    ham_list.append(Homega_list)
 
     for m in range(time_steps):
         # Filling the S matrix
@@ -170,7 +179,7 @@ for frag in range(len(ncas_sub)):
             #print("F_{}_{} = {}".format(m, n, Fmat_el))
             F_mat[m][n] = Fmat_el
             F_mat[n][m] = np.conj(Fmat_el)
-        #print(S_mat)
+        #print(F_mat)
 
         # Using an SVD to condition the F matrix
         # Before doing the eigendecomposition
@@ -188,24 +197,21 @@ for frag in range(len(ncas_sub)):
         eigvals, eigvecs = LA.eig(Fp)
 
         idx = eigvals.argsort()
-        print("Eigvals: ", eigvals)
         eigvals = eigvals[idx]
         print("Eigvals: ", eigvals)
         eigvecs = eigvecs[:,idx]
-        print("Eigvecs: ", eigvecs)
+        #print("Eigvecs: ", eigvecs)
     # QKSD Wavefunction
     qksd_wfn = []
+    c_list = []
     for m in range(time_steps):
-        coeff = eigvecs[0][m]
-        qksd_wfn.append(coeff*omega_list[m])
-    print("QKSD wavefunction: ", np.sum(qksd_wfn, axis=0))
-    wfn_list.append(np.sum(qksd_wfn, axis=0))
+        c_list.append(eigvecs[m][0])
+    print("QKSD wfn coefficients: ", c_list)
+    print("QKSD wfn: ", np.sum([np.dot(eigvecs[m][0], omega_list[m]) for m in range(time_steps)]))
+    coeff_list.append(c_list)
 
 print("Norms: ")
-print(np.linalg.norm(wfn_list[0]))
-print(np.linalg.norm(wfn_list[1]))
-wfn_list[0] = wfn_list[0]/np.linalg.norm(wfn_list[0])
-wfn_list[1] = wfn_list[1]/np.linalg.norm(wfn_list[1])
+print([np.linalg.norm(wfn_list[0][x]) for x in range(time_steps)])
 
 ## Whole-system QKSD
 
@@ -269,99 +275,70 @@ except:
     Hsp = get_scipy_csc_from_op(ham_mat)
     save_npz("h4_hsp_las.npz", Hsp)
 
-'''
-# Create a Hartree-Fock state
-init_state = HartreeFock(n_so, (n_alpha, n_beta), qubit_converter)
-backend = Aer.get_backend('statevector_simulator')
-job = execute(init_state, backend=backend, shots=1, memory=True)
-job_result = job.result()
-init_statevector = np.asarray(job_result.get_statevector(init_state)._data, dtype=complex)
-'''
-
-qr1 = QuantumRegister(np.sum(las.ncas_sub)*2, 'q1')
-new_circuit = QuantumCircuit(qr1)
-new_circuit.initialize(wfn_list[0], [0,1,4,5])
-new_circuit.initialize(wfn_list[1], [2,3,6,7])
-'''
-new_circuit.initialize(get_so_ci_vec(las.ci[0][0],2*las.ncas_sub[0],las.nelecas_sub[0]), [0,1,4,5])
-new_circuit.initialize(get_so_ci_vec(las.ci[1][0],2*las.ncas_sub[1],las.nelecas_sub[1]), [2,3,6,7])
-'''
-backend = Aer.get_backend('statevector_simulator')
-job = execute(new_circuit, backend=backend, shots=1, memory=True)
-job_result = job.result()
-init_statevector = np.asarray(job_result.get_statevector(new_circuit)._data, dtype=complex)
-
-'''
-# Alternative way to obtain the HF statevector
-bitstring = np.zeros(n_so)
-bitstring[:n_alpha] = 1
-bitstring[int(n_so/2):int(n_so/2)+n_beta] = 1
-bitstring = np.flip(bitstring)
-bitstring = ''.join(str(int(b)) for b in bitstring)
-init_statevector = np.zeros(2**n_so)
-init_statevector[int(bitstring, 2)] = 1
-print(np.nonzero(init_statevector))
-'''
-
-# Initialize F, S matrices
-F_mat = np.zeros((time_steps, time_steps), dtype=complex)
-S_mat = np.zeros((time_steps, time_steps), dtype=complex)
-
-# U |\phi_0>
-statevector = apply_time_evolution_op(init_statevector, Hsp, tau, time_steps)
-omega_list = [np.asarray(state, dtype=complex) for state in statevector]
-
-# V U |\phi_0>
-Homega_list = [Hsp.dot(omega) for omega in omega_list]
-
 en_list = []
+tot_coeffs_list = []
+F_tot = np.zeros((time_steps, time_steps), dtype=complex)
+S_tot = np.zeros((time_steps, time_steps), dtype=complex)
+for step1 in range(time_steps):
+    for step2 in range(time_steps):
+        frag0_state = wfn_list[0][step1]
+        frag1_state = wfn_list[1][step2]
+
+        init_statevector = np.kron(frag0_state, frag1_state) 
+
+        # Initialize F, S matrices
+        F_mat = np.zeros((time_steps, time_steps), dtype=complex)
+        S_mat = np.zeros((time_steps, time_steps), dtype=complex)
+
+        # U |\phi_0>
+        statevector = apply_time_evolution_op(init_statevector, Hsp, tau, time_steps)
+        omega_list = [np.asarray(coeff_list[0][step1]*coeff_list[1][step2]*state, dtype=complex) for state in statevector]
+
+        # V U |\phi_0>
+        Homega_list = [Hsp.dot(omega) for omega in omega_list]
+
+        for m in range(time_steps):
+            # Filling the S matrix
+            for n in range(m+1):
+                # < \phi_0 | U_m^+ U_n | \phi_0 >
+                Smat_el = np.vdot(omega_list[m], omega_list[n])
+
+                #print("S_{}_{} = {}".format(m, n, Smat_el))
+                S_mat[m][n] = Smat_el
+                S_mat[n][m] = np.conj(Smat_el)
+
+            # Filling the F matrix
+            # < \phi_0 | U_m^+ V U_n | \phi_0 >
+            for n in range(m+1):
+                Fmat_el = np.vdot(omega_list[m], Homega_list[n])
+                #print("F_{}_{} = {}".format(m, n, Fmat_el))
+                F_mat[m][n] = Fmat_el
+                F_mat[n][m] = np.conj(Fmat_el)
+            #print(S_mat)
+        F_tot += F_mat
+        S_tot += S_mat
+
 for m in range(time_steps):
-    # Filling the S matrix
-    for n in range(m+1):
-        # < \phi_0 | U_m^+ U_n | \phi_0 >
-        Smat_el = np.vdot(omega_list[m], omega_list[n])
-
-        #print("S_{}_{} = {}".format(m, n, Smat_el))
-        S_mat[m][n] = Smat_el
-        S_mat[n][m] = np.conj(Smat_el)
-
-    # Filling the F matrix
-    # < \phi_0 | U_m^+ V U_n | \phi_0 >
-    for n in range(m+1):
-        Fmat_el = np.vdot(omega_list[m], Homega_list[n])
-        #print("F_{}_{} = {}".format(m, n, Fmat_el))
-        F_mat[m][n] = Fmat_el
-        F_mat[n][m] = np.conj(Fmat_el)
-    #print(S_mat)
-
     # Using an SVD to condition the F matrix
     # Before doing the eigendecomposition
     Stol=1e-12
-    U, s, Vh = LA.svd(S_mat[:m+1, :m+1])
+    U, s, Vh = LA.svd(S_tot[:m+1, :m+1])
 
     #print(np.allclose(U, Vh.T.conj()))
     Dtemp = 1/np.sqrt(s)
     Dtemp[Dtemp**2 > 1/Stol] = 0
 
     Xp = U[0:len(s),0:len(Dtemp)]*Dtemp
-    Fp = Xp.T.conjugate() @ F_mat[:m+1,:m+1] @ Xp
+    Fp = Xp.T.conjugate() @ F_tot[:m+1,:m+1] @ Xp
 
     # Eigenvalues of the conditioned matrix
     eigvals, eigvecs = LA.eig(Fp)
-    '''
-    # Using the generalized Schur decomposition of F, S
-    # to obtain the eigvals via scipy.linalg.ordqz (ordered QZ)
-    AA, BB, alpha, beta, Q, Z = LA.ordqz(F_mat, S_mat, sort='lhp')
-    #AA, BB, Q, Z = LA.qz(F_mat, S_mat)
 
-    alpha = np.diag(AA)
-    beta = np.diag(BB)
-    #print("Alpha: ",alpha)
-    #print("Beta: ",beta)
-    eigvals = alpha/beta
-    '''
-    #print("eigvals = ", np.sort(eigvals))
-    print("QKSD energy = ", eigvals[0])
-    en_list.append(eigvals[0])
+    idx = eigvals.argsort()
+    eigvals = eigvals[idx]
+    print("Eigvals: ", eigvals)
+    eigvecs = eigvecs[:,idx]
+    print("QKSD energy",m+1,"  = ", eigvals[0])
+    tot_coeffs = eigvecs[:, 0]
 
-    print("Error for timestep={}: {}".format(m+1, eigvals[0]-np_en))
+print("Total energy: ", eigvals[0])
